fd673707aeb24cd114b894c9ea8c9a1551d06c0f
hadil kharroubi <hadilkharroubi85@gmail.com>
2025-12-09 01:15:24 +0100

    fourth step

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Search } from "../search/search";
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [Search],
   templateUrl: './main.html',
   styleUrl: './main.scss',
 })
